f33af0d0ed9a447d82001c4f956cf50902f50d13
r0ttingm0on <77366598+r0ttingm0on@users.noreply.github.com>
2021-03-03 14:13:11 -0700

    Add files via upload

---
+++ c.patterson toolkit.py
@@ -0,0 +1,17 @@
+#basic for loop
+for x in range(0, 10):
+    print("hello world!")
+#basic list
+myList = []
+myList.append("apples")
+myList.append("pineapples")
+print (myList)
+
+#basic conditional
+x = int(x)
+x = input("type a number")
+if x > 0:
+    print("thats a small number!")
+else:
+    print("thats more than nothing!")
+              
